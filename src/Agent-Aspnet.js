a0947eccf81919d4f1bbd4e81bd4db30fbb00b6f
imdark <mikesgoacademy@gmail.com>
2014-06-24 09:24:54 +0300

    Added Aspnet Agent

---
+++ src/Agent-Aspnet.js
@@ -0,0 +1,25 @@
+
+/*global define */
+
+define(function AgentAspnet(require, exports, module) {
+    'use strict';
+//    exports.init = init;
+//    exports.isReady = isReady;
+//    exports.id = "agent-aspnet";
+//
+//    // satisfied from locally cached data (sync)
+//    exports.functionWithId = functionWithId;
+//    exports.functionsInFile = functionsInFile;
+//    exports.probesInFile = probesInFile;
+//    exports.cachedHitCounts = cachedHitCounts;
+//
+//    // fetch data from the instrumented app (async)
+//    exports.refreshHitCounts = refreshHitCounts;
+//    exports.refreshExceptionCounts = refreshExceptionCounts;
+//    exports.trackLogs = trackLogs;
+//    exports.refreshLogs = refreshLogs;
+//
+//    exports.backtrace = backtrace;
+//
+//    exports.resetTrace = resetTrace;
+});
